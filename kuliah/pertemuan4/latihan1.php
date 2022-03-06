3be728f30db53b28167ef770d6e520e5bc29379c
Divanurh <divanurhaffifah@gmail.com>
2022-03-06 22:59:14 +0700

    pertemuan4

---
+++ kuliah/pertemuan4/latihan1.php
@@ -0,0 +1,44 @@
+<?php
+//FUNCTION
+
+// Built-in Function
+// date();
+echo date("l, j F  Y");
+echo "<hr>";
+
+// time()
+echo time();
+// UNIX Timestamp / EPOCH Time
+// Detik yang sudah berlalu sejak 1 Januari 1970
+echo"<br>";
+echo time() + 60 * 60 * 24;
+echo "<hr>";
+echo date("l, j F  Y", time( ) + 60 * 60 * 24 * 100);
+echo "<hr>";
+// mktime (0,0,0,0,0,0);
+// mendapatkan detik pada tanggal tertentu
+// jam,menit,detik,bulan,tanggal tahun
+echo mktime(0,0,0,3,5,2022);
+echo "<hr>";
+echo date("l", mktime (0,0,0,1,5,2003));
+echo "<hr>";
+
+//MATH
+echo pow(2,3);
+echo "<br>";
+echo rand(1,10);
+echo "<br>";
+// pembulatan
+// round (), ceil(), floor();
+echo round(2.6);   //terdekat
+echo "<br>";
+echo ceil(2.1);    //keatas (langit2)
+echo "<br>";
+echo floor (2.9);  //kebawah
+echo "<hr>";
+
+
+
+
+
+?>
---
+++ kuliah/pertemuan4/latihan2.php
@@ -0,0 +1,17 @@
+<?php
+// Membuat fungsi / definisi fungsi
+function totalLuasDuaKubus($a, $b) {
+    $luas_a = $a * $a * $a;  // bisa pke pow($a,3)
+    $luas_b = $b * $b * $b;
+
+    $total = $luas_a + $luas_b;
+
+    return "Total luas dari kubus A dengan sisi $a dan kubus B dengan sisi $b adalah $total";
+}
+
+echo totalLuasDuaKubus(9,4);
+echo "<br>";
+echo totalLuasDuaKubus(10,15);
+echo "<br>";
+echo totalLuasDuaKubus(100,200);
+?>
